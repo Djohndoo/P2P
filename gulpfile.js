e2b8b49c9034477c9f58ece2b10eac6a2e49a959
Unknown <acourtin89@gmail.com>
2017-12-20 13:26:02 +0100

    [INIT]

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    stylus = require('gulp-stylus'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browser_sync = require('browser-sync'),
+    reload = browser_sync.reload;
+
+    gulp.task('stylus', function () {
+        return gulp.src('./src/stylus/app.styl')
+          .pipe(sourcemaps.init())
+          .pipe(stylus())
+          .pipe(postcss([
+              autoprefixer()
+          ]))
+          .pipe(sourcemaps.write('./'))
+          .pipe(gulp.dest('./dist/css/'))
+          .pipe(reload({stream:true}));
+      });
+
+      gulp.task('serve', function() {
+          browser_sync({
+              server: {
+                  baseDir: './'
+              }
+          });
+          gulp.watch(['./src/stylus/*/*.styl', './src/stylus/*.styl'], ['stylus'])
+      });
